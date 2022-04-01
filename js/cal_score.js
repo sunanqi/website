906e540758795641f7f218f97f07527cf139c3c3
anqi <58937327+hcr2@users.noreply.github.com>
2022-04-01 03:13:12 -0700

    Add files via upload

--- js/cal_score.js
+++ js/cal_score.js
@@ -3,14 +3,14 @@ function cal_score(n_stages, score_cutoffs) {
   for (i = 0; i < n_stages; i++) {
     result = parseFloat(document.getElementById('num'+i.toString()).value);
     if (!isNaN(result)) {
-      if (score_cutoffs[i][0] == 'm') {
-        if (result >= score_cutoffs[i][2]) score = 10000;
-        else if (result <= score_cutoffs[i][1]) score = 0;
-        else score = Math.round((result/score_cutoffs[i][2])**2*9000+1000);
-      } else if (score_cutoffs[i][0] == 's') {
-        if (result <= score_cutoffs[i][1]) score = 10000;
-        else if (result >= score_cutoffs[i][2]) score = 1000;
-        else score = Math.round(((score_cutoffs[i][2]-result)/(score_cutoffs[i][2]-score_cutoffs[i][1]))**2*9000+1000);
+      if (score_cutoffs[3*i] == 1) {
+        if (result >= score_cutoffs[3*i+2]) score = 10000;
+        else if (result == 0) score = 0;
+        else score = Math.round((result/score_cutoffs[3*i+2])**2*9000+1000);
+      } else if (score_cutoffs[3*i] == 2) {
+        if (result <= score_cutoffs[3*i+2]) score = 10000;
+        else if (result >= score_cutoffs[3*i+1]) score = 1000;
+        else score = Math.round(((score_cutoffs[3*i+1]-result)/(score_cutoffs[3*i+1]-score_cutoffs[3*i+2]))**2*9000+1000);
       } else score=0;
       total += score;
       document.getElementById('score'+i.toString()).value = score;
